1f98aeebdb061625e150220b5493da3b046f4af1
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-07-20 08:01:13 +0530

    Update tutorial.py

--- plugins/tutorial.py
+++ plugins/tutorial.py
@@ -2,7 +2,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 
 @Client.on_message(Filters.command(["tute"]))
 async def start(client, message):
-    await message.reply_text("To make a YouTube downloader pess ⚜️ Go ⚜️ button to go youtube directly. 🥳",
+    await message.reply_text("To make a YouTube downloader pess ⚜️ Go whatsapp bot make ⚜️ button to go youtube directly to make whatsapp bot. 🥳",
                              reply_markup=InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("⚜️ Go whatsapp bot make⚜️", url="https://youtu.be/_lGtWG8zTdU")]
