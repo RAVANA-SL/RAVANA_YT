a40e2f93e9d2bc89beae9904fe2061fb43fd4535
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-07-20 08:09:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,12 +6,12 @@ async def start(client, message):
     # return
     Lasiya = InlineKeyboardMarkup([
         
-        [InlineKeyboardButton("Youtube ❤", url="https://www.youtube.com/channel/UCHyseVcfusXkOClpwja00yg")],
+        [InlineKeyboardButton("Youtube ❤", url="https://youtube.com/channel/UC4WaTaXOPPFP3V6sDBogJug")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/Danuma_admin_bot")],
+            "Report Bugs 😊", url="https://t.me/RAVANA123_BOT")],
         [InlineKeyboardButton(
-            "Bot channel 🧪",url="https://t.me/danumabots")]
+            "Bot channel 🧪",url="https://t.me/ravanabots")]
     ])
-    thumbnail_url = "https://telegra.ph/file/69a96df53932f1cd2174f.jpg"
+    thumbnail_url = "https://telegra.ph/file/24cacd9938446a38180fd.png"
     await message.reply_photo(thumbnail_url, caption=f"Hi<b>{message.from_user.first_name}</b>\n\n<b>Instructions for use..</b>\n• Type /help to get instructins.\n• Type /tute for make a bot like me.\n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Lasiya)
     raise StopPropagation
